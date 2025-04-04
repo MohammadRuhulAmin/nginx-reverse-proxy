224e10643c07a893029c842cc49050c34686841d
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2025-04-04 13:15:56 +0600

    nodejs and docker clustarization setup done

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const replicaApp = process.env.APP_NAME;
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  console.log(`requested server : ${replicaApp}`)
+});
+
+app.listen(port, () => {
+  console.log(`${replicaApp}  listening on port ${port}`);
+});
